e9c3c14ca04ccab96f0eb78aa73c3454e1d83411
farooq azam khan <farooq1.khan@ryerson.ca>
2019-12-25 16:07:55 -0500

    Updates

---
+++ precache-manifest.15b73691b13865ec32f25c486a245bf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a21e7d0a79d50cd9fd1b43d007f431bb",
+    "url": "/shopping-app/index.html"
+  },
+  {
+    "revision": "deee79276ed56937c9a5",
+    "url": "/shopping-app/static/css/main.07a718d4.chunk.css"
+  },
+  {
+    "revision": "3c58ac1a97972e07e2c8",
+    "url": "/shopping-app/static/js/2.a7fd8bd2.chunk.js"
+  },
+  {
+    "revision": "deee79276ed56937c9a5",
+    "url": "/shopping-app/static/js/main.466bcb93.chunk.js"
+  },
+  {
+    "revision": "f9076af2923116c8cc39",
+    "url": "/shopping-app/static/js/runtime~main.760526e6.js"
+  }
+]);
